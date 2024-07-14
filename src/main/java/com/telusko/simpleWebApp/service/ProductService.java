5640ac3fcf4050587af22db1047a24f2cf5a132a
ArupMathura <arupmathura06@gmail.com>
2024-07-14 10:54:40 +0530

    get product by id implementation.

--- src/main/java/com/telusko/simpleWebApp/service/ProductService.java
+++ src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -1,11 +1,13 @@
 package com.telusko.simpleWebApp.service;
 
 import com.telusko.simpleWebApp.model.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductService {
     List<Product> products = Arrays.asList(
@@ -16,5 +18,16 @@ public class ProductService {
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public Product getProductById(int prodId) {
+
+        return products.stream()
+                .filter(p -> {
+                    log.debug("Checking product with prodId: {}", p.getProdId());
+                    return p.getProdId() == prodId;
+                })
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Product with id " + prodId + " not found"));
     }
 }
