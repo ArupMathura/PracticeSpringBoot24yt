25469916da68b172e2193749b8795280bcf63080
ArupMathura <arupmathura06@gmail.com>
2024-07-14 11:51:17 +0530

    add operation - POST operation

--- src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -1,14 +1,12 @@
 package com.telusko.simpleWebApp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Product {
     int prodId;
     String prodName;
--- src/main/java/com/telusko/simpleWebApp/service/ProductService.java
+++ src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -4,17 +4,18 @@ import com.telusko.simpleWebApp.model.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @Service
 public class ProductService {
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "OnePlus", 35000),
             new Product(102, "iPhone", 70000),
             new Product(103, "macbook", 100000)
-    );
+    ));
 
     public List<Product> getProducts() {
         return products;
@@ -24,10 +25,16 @@ public class ProductService {
 
         return products.stream()
                 .filter(p -> {
-                    log.debug("Checking product with prodId: {}", p.getProdId());
+                    log.info("Checking product with prodId: {}", p.getProdId());
+                    log.info("Checking product with prodId: {}", prodId);
                     return p.getProdId() == prodId;
                 })
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Product with id " + prodId + " not found"));
+    }
+
+    public void addProduct(Product product) {
+        System.out.println(product);
+        products.add(product);
     }
 }
