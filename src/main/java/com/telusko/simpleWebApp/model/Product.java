45d9a9e70d7ccd228a6e8a81ffca2b9536dffc76
ArupMathura <arupmathura06@gmail.com>
2024-07-15 18:34:57 +0530

    add generated value in Product to automatically get id

--- src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -1,6 +1,8 @@
 package com.telusko.simpleWebApp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -13,6 +15,7 @@ import lombok.*;
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int prodId;
     String prodName;
     int price;
